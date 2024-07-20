cc653d128f070ce80dbf8d21bf19dcca98864885
Arthur <arthurferreira1417@gmail.com>
2024-07-19 21:51:53 -0300

    Faet: Criando o middleware de validação de token

---
+++ api/app/Http/Middleware/JwtValidation.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class JwtValidation
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    
+    private $key;
+
+    public function __construct(){
+        $this->key = env('JWT');
+
+    }
+
+    public function handle(Request $request, Closure $next){
+
+        $data = $this->getToken($request);
+        if(!$data){
+            error('header não presente no request', 401);
+        }
+        $token = $this->validToken($data);
+        if(!$token){
+            error('token invalido ou expirado', 403);
+        }
+        $request = merge(['auth' => (array) $token]);
+        return $next($request);
+    }
+    public function validToken($token){
+        
+        list($header, $payload, $sing) = explode('.', $token);
+        
+        $decPayload = json_decode(base64_decode($payload));
+        $validSing = base64_encode(hash_hmac('sha256', $header . "." . $payload, $this->key, true));
+
+        if($sing !== $validSing){
+            return false;
+        }
+        if($decPayload->exp < time()){
+            return false;
+        }
+        return $decPayload;
+        
+
+    }
+
+    public function getToken($request){
+        $data = $request->header('Authorization');
+        
+        if (empty($data)) {
+            return false;
+        }
+        $token = explode(' ', $data);
+        return $token[1];
+    }
+}
