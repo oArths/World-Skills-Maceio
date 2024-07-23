9a6b7e37c0a13c3d04a91310c88ca779007db8af
Arthur <arthurferreira1417@gmail.com>
2024-07-23 16:12:13 -0300

    Feat: criando as rotas

--- api/routes/api.php
+++ api/routes/api.php
@@ -21,5 +21,17 @@ Route::prefix('AlatechMachines/api')->group(function(){
     Route::delete('/machines/{id?}', [AllController::class, 'removeMachine'])->middleware('jwt');
     Route::get('/images/{id?}', [AllController::class, 'getImages'])->middleware('jwt');
     Route::get('/list', [AllController::class, 'ListItems'])->middleware('jwt');
+
+    Route::get('/list/motherboards', [AllController::class, 'Listmotherboards'])->middleware('jwt');
+    Route::get('/list/processor', [AllController::class, 'Listprocessor'])->middleware('jwt');
+    Route::get('/list/rammemory', [AllController::class, 'Listrammemory'])->middleware('jwt');
+    Route::get('/list/storagedevice', [AllController::class, 'Liststoragedevice'])->middleware('jwt');
+    Route::get('/list/graphiccard', [AllController::class, 'Listgraphiccard'])->middleware('jwt');
+    Route::get('/list/powersupply', [AllController::class, 'Listpowersupply'])->middleware('jwt');
+    Route::get('/list/brand', [AllController::class, 'Listbrand'])->middleware('jwt');
+    Route::get('/list/machine', [AllController::class, 'Listmachine'])->middleware('jwt');
+
+
     Route::get('/search/{category?}/{q?}/{pagesize?}/{page?}', [AllController::class, 'Searchitem'])->middleware('jwt');
+    Route::post('/verify-Compatibility', [AllController::class, 'VerifyComp'])->middleware('jwt');
 });
